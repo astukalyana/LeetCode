6022b58534184b284af42991479f511f093096f2
astukalyana <astukalyana@gmail.com>
2023-09-26 15:28:13 +0700

    Added the solution to 1 Leetcode easy tagged question: (1) 392. Is Subsequence

---
+++ codes/easy/392 Is Subsequence/mySolution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = 0
+        j = 0
+
+        while(j < len(t)):
+            if(i < len(s) and s[i] == t[j]):
+                i += 1
+            j += 1
+        
+        return i == len(s)
